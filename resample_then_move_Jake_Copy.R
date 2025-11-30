8135b53d5f547363f4f287c3dc2b80f4e8518a50
JakeBris <Jake.Brisnehan@colostate.edu>
2025-11-30 16:11:52 -0700

    Double Move Step files and new code

---
+++ resample_then_move_Jake_Copy.R
@@ -0,0 +1,22 @@
+#-----------------------------------------------------------------------------------------------#
+# Script Name: resample_the_move                                                                #
+# Author: Hayden Moffat                                                                         #
+# email: hayden.moffat@hdr.qut.edu.au                                                           #
+#                                                                                               #
+# This R script calls the scripts/functions for the resampling and move steps involved in SMC.  #                                                           
+#                                                                                               #                                                                                           
+#-----------------------------------------------------------------------------------------------#
+
+#This is not the original and pulls from new move step file so that two steps are used
+
+# Resample and move step
+theta[,,M] <- residual_resampling(W[,M], N, theta[,,M]) 
+source('move_step_Jake_Copy.R') #JB: uses my copy
+
+# Determine the number of unique particles
+n_unique = theta[,,M] %>% as.data.frame %>% as.tbl %>% distinct %>% nrow # d
+print(paste("Number of unique particles after resampling:", n_unique))
+
+# Reset weights and ESS
+W[,M] = 1/N
+ESS[M] = N
